11cbb4976c40c86b2f127ae1eeed9d1a933b7c83
mattgefen <matt.gefen@gmail.com>
2021-12-03 13:48:10 -0700

    removing html and rendering imagesArr in App.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,21 @@ import imagesArr from "./imageData";
 
 export default function App() {
   // USE useState TO CREATE  [bigImage, setBigImage]
+  const [bigImage, setBigImage] = useState(imagesArr[0].img)
   // AND SET IT TO THE IMAGE URL OF THE FIRST ELEMENT IN THE ARRAY
-
+  
   // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
+  const handleClick = (imgUrl) => {
+    setBigImage(imgUrl)
+  }
   // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
 
   // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
+
+  const images = imagesArr.map((element, index) => {
+    return <img src={element.img} alt={element.city} key={index} className='thumb' onClick={handleClick}/>
+  })
+
   // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key
   // ALSO ASSIGN AN onClick EVENT THAT CALL THE HANDLE EVENT AND PASSES IT THE IMG URL
 
@@ -19,7 +28,7 @@ export default function App() {
     <div className="App">
       <h1>Cities Of The World</h1>
       <div id="wrapper">
-        <div id="thumbnails">{/* RENDER THE IMAGES ARRAY  */}</div>
+        <div id="thumbnails">{/* RENDER THE IMAGES ARRAY  */}{images}</div>
         {/* THE SRC IMAGE URL SHOULD BE SET TO THE VALUE THAT IS STORED IN bigImage */}
         {/* <img src="" id="bigimage" alt='bigImaage'/> */}
       </div>
